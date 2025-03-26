f0746d376fb84b64e852a86ef884f09a216f8fc0
Arno-Willaert <Arno.willaert@student.tsaam.be>
2025-03-26 11:43:18 +0100

    commit1

--- script/script.js
+++ script/script.js
@@ -20,4 +20,38 @@ const users = [
   },
 ];
 
+
+
 // hieronder komt jullie code - veel succes
+
+
+//html elementen registratie
+const naam = document.querySelector("#name")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+
+
+// nieuwe registratie
+document.querySelector("#registreer").addEventListener("click", () =>{
+  const nieuweGebruiker = {
+    fullname: naam.value,
+    email: email.value,
+    password: password.value,
+  }
+  if(naam == none || email== none || password == none){
+    console.log("er mist een element");
+  }
+  users.push(nieuweGebruiker)
+  console.log(users);
+  
+})
+
+
+
+//htlm elementen filter
+const filter = document.querySelector("#filter")
+document.querySelector("#filterBtn").addEventListener("click",()=>{
+  if (filter.value< 4){
+    alert("minimum 4 letters aub")
+  }
+})
